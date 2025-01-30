2f1367edaa87a9a39a9b39e39b4e0d1f9629161a
Sakthivel <73012840+sakthivel027@users.noreply.github.com>
2025-01-30 22:47:53 +0530

    Create models.py
    added

---
+++ models.py
@@ -0,0 +1,238 @@
+models.py
+</select>
+<div class="styled-select-active-helper"></div>
+</div>
+<i class="sprite svg-arrow-down"></i>
+</div>
+</div>
+<button class="pure-button pure-button-primary-progressive" type="submit">
+<i class="sprite svg-search-icon" data-jsl10n="search-input-button">Search</i>
+</button>
+<input type="hidden" value="Go" name="go">
+</fieldset>
+</form>
+</div>
+<div class="lang-list-button-wrapper">
+<button id="js-lang-list-button" class="lang-list-button">
+<i class="sprite svg-translate-icon"></i>
+<span class="lang-list-button-text jsl10n" data-jsl10n="language-button-text">Read Wikipedia in your language </span>
+<i class="sprite svg-arrow-down-blue"></i>
+</button>
+</div>
+<div class="lang-list-border"></div>
+<div class="lang-list-container">
+<div id="js-lang-lists" class="lang-list-content">
+<!-- Bookshelves -->
+<h2 class="bookshelf-container">
+<span class="bookshelf">
+<span class="text">
+<bdi dir="ltr">
+1&nbsp;000&nbsp;000+
+</bdi>
+<span class="jsl10n" data-jsl10n="entries">
+articles
+</span>
+</span>
+</span>
+</h2>
+<!-- 1,000,000+ content pages -->
+<div class="langlist langlist-large hlist" data-el-section="secondary links">
+<ul>
+<li><a href="//ar.wikipedia.org/" lang="ar" title="Al-Ê¿ArabÄ«yah"><bdi dir="rtl">Ø§Ù„Ø¹Ø±Ø¨ÙŠØ©</bdi></a></li>
+<li><a href="//de.wikipedia.org/" lang="de">Deutsch</a></li>
+<li><a href="//en.wikipedia.org/" lang="en" title="English">English</a></li>
+<li><a href="//es.wikipedia.org/" lang="es">EspaÃ±ol</a></li>
+<li><a href="//fr.wikipedia.org/" lang="fr">FranÃ§ais</a></li>
+<li><a href="//it.wikipedia.org/" lang="it">Italiano</a></li>
+<li><a href="//nl.wikipedia.org/" lang="nl">Nederlands</a></li>
+<li><a href="//ja.wikipedia.org/" lang="ja" title="Nihongo">æ—¥æœ¬èªž</a></li>
+<li><a href="//pl.wikipedia.org/" lang="pl">Polski</a></li>
+<li><a href="//pt.wikipedia.org/" lang="pt">PortuguÃªs</a></li>
+<li><a href="//ru.wikipedia.org/" lang="ru" title="Russkiy">Ð ÑƒÑÑÐºÐ¸Ð¹</a></li>
+<li><a href="//ceb.wikipedia.org/" lang="ceb">Sinugboanong Binisaya</a></li>
+<li><a href="//sv.wikipedia.org/" lang="sv">Svenska</a></li>
+<li><a href="//vi.wikipedia.org/" lang="vi">Tiáº¿ng Viá»‡t</a></li>
+<li><a href="//war.wikipedia.org/" lang="war">Winaray</a></li>
+<li><a href="//zh.wikipedia.org/" lang="zh" title="ZhÅngwÃ©n">ä¸­æ–‡</a></li>
+</ul>
+</div>
+<!-- Bookshelves -->
+<h2 class="bookshelf-container">
+<span class="bookshelf">
+<span class="text">
+<bdi dir="ltr">
+100&nbsp;000+
+</bdi>
+<span class="jsl10n" data-jsl10n="entries">
+articles
+</span>
+</span>
+</span>
+</h2>
+<!-- 100,000+ content pages -->
+<div class="langlist langlist-large hlist" data-el-section="secondary links">
+<ul>
+<li><a href="//az.wikipedia.org/" lang="az">AzÉ™rbaycanca</a></li>
+<li><a href="//bg.wikipedia.org/" lang="bg" title="BÇŽlgarski">Ð‘ÑŠÐ»Ð³Ð°Ñ€ÑÐºÐ¸</a></li>
+<li><a href="//zh-min-nan.wikipedia.org/" lang="nan">BÃ¢n-lÃ¢m-gÃº / HÅ-lÃ³-oÄ“</a></li>
+<li><a href="//be.wikipedia.org/" lang="be" title="Belaruskaya (Akademichnaya)">Ð‘ÐµÐ»Ð°Ñ€ÑƒÑÐºÐ°Ñ (ÐÐºÐ°Ð´ÑÐ¼Ñ–Ñ‡Ð½Ð°Ñ)</a></li>
+<li><a href="//ca.wikipedia.org/" lang="ca">CatalÃ </a></li>
+<li><a href="//cs.wikipedia.org/" lang="cs" title="ÄeÅ¡tina">ÄŒeÅ¡tina</a></li>
+<li><a href="//da.wikipedia.org/" lang="da">Dansk</a></li>
+<li><a href="//et.wikipedia.org/" lang="et">Eesti</a></li>
+<li><a href="//el.wikipedia.org/" lang="el" title="EllÄ«nikÃ¡">Î•Î»Î»Î·Î½Î¹ÎºÎ¬</a></li>
+<li><a href="//eo.wikipedia.org/" lang="eo">Esperanto</a></li>
+<li><a href="//eu.wikipedia.org/" lang="eu">Euskara</a></li>
+<li><a href="//fa.wikipedia.org/" lang="fa" title="FÄrsi"><bdi dir="rtl">ÙØ§Ø±Ø³ÛŒ</bdi></a></li>
+<li><a href="//gl.wikipedia.org/" lang="gl">Galego</a></li>
+<li><a href="//hy.wikipedia.org/" lang="hy" title="Hayeren">Õ€Õ¡ÕµÕ¥Ö€Õ¥Õ¶</a></li>
+<li><a href="//hi.wikipedia.org/" lang="hi" title="HindÄ«">à¤¹à¤¿à¤¨à¥à¤¦à¥€</a></li>
+<li><a href="//hr.wikipedia.org/" lang="hr">Hrvatski</a></li>
+<li><a href="//id.wikipedia.org/" lang="id">Bahasa Indonesia</a></li>
+<li><a href="//he.wikipedia.org/" lang="he" title="Ê¿Ivrit"><bdi dir="rtl">×¢×‘×¨×™×ª</bdi></a></li>
+<li><a href="//ka.wikipedia.org/" lang="ka" title="Kartuli">áƒ¥áƒáƒ áƒ—áƒ£áƒšáƒ˜</a></li>
+<li><a href="//la.wikipedia.org/" lang="la">Latina</a></li>
+<li><a href="//lt.wikipedia.org/" lang="lt">LietuviÅ³</a></li>
+<li><a href="//hu.wikipedia.org/" lang="hu">Magyar</a></li>
+<li><a href="//mk.wikipedia.org/" lang="mk" title="Makedonski">ÐœÐ°ÐºÐµÐ´Ð¾Ð½ÑÐºÐ¸</a></li>
+<li><a href="//ms.wikipedia.org/" lang="ms">Bahasa Melayu</a></li>
+<li><a href="//min.wikipedia.org/" lang="min">Bahaso Minangkabau</a></li>
+<li lang="no">Norsk<ul><li><a href="//no.wikipedia.org/" lang="nb">bokmÃ¥l</a></li><li><a href="//nn.wikipedia.org/" lang="nn">nynorsk</a></li></ul></li>
+<li><a href="//ce.wikipedia.org/" lang="ce" title="NoxÃ§iyn">ÐÐ¾Ñ…Ñ‡Ð¸Ð¹Ð½</a></li>
+<li><a href="//uz.wikipedia.org/" lang="uz">OÊ»zbekcha / ÐŽÐ·Ð±ÐµÐºÑ‡Ð°</a></li>
+<li><a href="//kk.wikipedia.org/" lang="kk"><span lang="kk-Cyrl">ÒšÐ°Ð·Ð°Ò›ÑˆÐ°</span> / <span lang="kk-Latn">QazaqÅŸa</span> / <bdi lang="kk-Arab" dir="rtl">Ù‚Ø§Ø²Ø§Ù‚Ø´Ø§</bdi></a></li>
+<li><a href="//ro.wikipedia.org/" lang="ro">RomÃ¢nÄƒ</a></li>
+<li><a href="//cy.wikipedia.org/" lang="cy" title="Saesneg">Cymraeg</a></li>
+<li><a href="//simple.wikipedia.org/" lang="en">Simple English</a></li>
+<li><a href="//sk.wikipedia.org/" lang="sk">SlovenÄina</a></li>
+<li><a href="//sl.wikipedia.org/" lang="sl">SlovenÅ¡Äina</a></li>
+<li><a href="//sr.wikipedia.org/" lang="sr">Ð¡Ñ€Ð¿ÑÐºÐ¸ / Srpski</a></li>
+<li><a href="//sh.wikipedia.org/" lang="sh">Srpskohrvatski / Ð¡Ñ€Ð¿ÑÐºÐ¾Ñ…Ñ€Ð²Ð°Ñ‚ÑÐºÐ¸</a></li>
+<li><a href="//fi.wikipedia.org/" lang="fi" title="suomi">Suomi</a></li>
+<li><a href="//ta.wikipedia.org/" lang="ta" title="Tamiá¸»">à®¤à®®à®¿à®´à¯</a></li>
+<li><a href="//th.wikipedia.org/" lang="th" title="Phasa Thai">à¸ à¸²à¸©à¸²à¹„à¸—à¸¢</a></li>
+<li><a href="//tg.wikipedia.org/" lang="tg" title="TojikÄ«">Ð¢Ð¾Ò·Ð¸ÐºÓ£</a></li>
+<li><a href="//azb.wikipedia.org/" lang="azb" title="TÃ¼rkce"><bdi dir="rtl">ØªÛ†Ø±Ú©Ø¬Ù‡</bdi></a></li>
+<li><a href="//tr.wikipedia.org/" lang="tr" title="TÃ¼rkÃ§e">TÃ¼rkÃ§e</a></li>
+<li><a href="//uk.wikipedia.org/" lang="uk" title="Ukrayinsâ€™ka">Ð£ÐºÑ€Ð°Ñ—Ð½ÑÑŒÐºÐ°</a></li>
+<li><a href="//ur.wikipedia.org/" lang="ur" title="Urdu"><bdi dir="rtl">Ø§Ø±Ø¯Ùˆ</bdi></a></li>
+<li><a href="//vo.wikipedia.org/" lang="vo">VolapÃ¼k</a></li>
+<li><a href="//ko.wikipedia.org/" lang="ko" title="Hangugeo">í•œêµ­ì–´</a></li>
+</ul>
+</div>
+<!-- Bookshelves -->
+<h2 class="bookshelf-container">
+<span class="bookshelf">
+<span class="text">
+<bdi dir="ltr">
+10&nbsp;000+
+</bdi>
+<span class="jsl10n" data-jsl10n="entries">
+articles
+</span>
+</span>
+</span>
+</h2>
+<!-- 10,000+ content pages -->
+<div class="langlist hlist" data-el-section="secondary links">
+<ul>
+<li><a href="//ace.wikipedia.org/" lang="ace">Bahsa AcÃ¨h</a></li>
+<li><a href="//af.wikipedia.org/" lang="af">Afrikaans</a></li>
+<li><a href="//als.wikipedia.org/" lang="gsw">Alemannisch</a></li>
+<li><a href="//am.wikipedia.org/" lang="am" title="Ä€mariÃ±Ã±Ä">áŠ áˆ›áˆ­áŠ›</a></li>
+<li><a href="//an.wikipedia.org/" lang="an">AragonÃ©s</a></li>
+<li><a href="//ast.wikipedia.org/" lang="ast">Asturianu</a></li>
+<li><a href="//bn.wikipedia.org/" lang="bn" title="Bangla">à¦¬à¦¾à¦‚à¦²à¦¾</a></li>
+<li><a href="//map-bms.wikipedia.org/" lang="map-x-bms">Basa Banyumasan</a></li>
+<li><a href="//ba.wikipedia.org/" lang="ba" title="BaÅŸqortsa">Ð‘Ð°ÑˆÒ¡Ð¾Ñ€Ñ‚ÑÐ°</a></li>
+<li><a href="//be-tarask.wikipedia.org/" lang="be" title="Belaruskaya (TaraÅ¡kievica)">Ð‘ÐµÐ»Ð°Ñ€ÑƒÑÐºÐ°Ñ (Ð¢Ð°Ñ€Ð°ÑˆÐºÐµÐ²Ñ–Ñ†Ð°)</a></li>
+<li><a href="//bpy.wikipedia.org/" lang="bpy" title="Bishnupriya Manipuri">à¦¬à¦¿à¦·à§à¦£à§à¦ªà§à¦°à¦¿à¦¯à¦¼à¦¾ à¦®à¦£à¦¿à¦ªà§à¦°à§€</a></li>
+<li><a href="//bar.wikipedia.org/" lang="bar">Boarisch</a></li>
+<li><a href="//bs.wikipedia.org/" lang="bs">Bosanski</a></li>
+<li><a href="//br.wikipedia.org/" lang="br">Brezhoneg</a></li>
+<li><a href="//cv.wikipedia.org/" lang="cv" title="ÄŒÄƒvaÅ¡la">Ð§Ó‘Ð²Ð°ÑˆÐ»Ð°</a></li>
+<li><a href="//nv.wikipedia.org/" lang="nv">DinÃ© Bizaad</a></li>
+<li><a href="//eml.wikipedia.org/" lang="roa-x-eml">EmigliÃ nâ€“RumagnÃ²l</a></li>
+<li><a href="//fo.wikipedia.org/" lang="fo">FÃ¸royskt</a></li>
+<li><a href="//fy.wikipedia.org/" lang="fy">Frysk</a></li>
+<li><a href="//ga.wikipedia.org/" lang="ga">Gaeilge</a></li>
+<li><a href="//gd.wikipedia.org/" lang="gd">GÃ idhlig</a></li>
+<li><a href="//gu.wikipedia.org/" lang="gu" title="Gujarati">àª—à«àªœàª°àª¾àª¤à«€</a></li>
+<li><a href="//hsb.wikipedia.org/" lang="hsb">Hornjoserbsce</a></li>
+<li><a href="//io.wikipedia.org/" lang="io">Ido</a></li>
+<li><a href="//ilo.wikipedia.org/" lang="ilo">Ilokano</a></li>
+<li><a href="//ia.wikipedia.org/" lang="ia">Interlingua</a></li>
+<li><a href="//os.wikipedia.org/" lang="os" title="Iron Ã†vzag">Ð˜Ñ€Ð¾Ð½ Ã¦Ð²Ð·Ð°Ð³</a></li>
+<li><a href="//is.wikipedia.org/" lang="is">Ãslenska</a></li>
+<li><a href="//jv.wikipedia.org/" lang="jv">Jawa</a></li>
+<li><a href="//kn.wikipedia.org/" lang="kn" title="Kannada">à²•à²¨à³à²¨à²¡</a></li>
+<li><a href="//ht.wikipedia.org/" lang="ht">KreyÃ²l Ayisyen</a></li>
+<li><a href="//ku.wikipedia.org/" lang="ku"><span lang="ku-Latn">KurdÃ®</span> / <bdi lang="ku-Arab" dir="rtl">ÙƒÙˆØ±Ø¯ÛŒ</bdi></a></li>
+<li><a href="//ckb.wikipedia.org/" lang="ckb" title="KurdÃ®y NawendÃ®"><bdi dir="rtl">Ú©ÙˆØ±Ø¯ÛŒÛŒ Ù†Ø§ÙˆÛ•Ù†Ø¯ÛŒ</bdi></a></li>
+<li><a href="//ky.wikipedia.org/" lang="ky" title="KyrgyzÄa">ÐšÑ‹Ñ€Ð³Ñ‹Ð·Ñ‡Ð°</a></li>
+<li><a href="//mrj.wikipedia.org/" lang="mjr" title="Kyryk Mary">ÐšÑ‹Ñ€Ñ‹Ðº ÐœÐ°Ñ€Ñ‹</a></li>
+<li><a href="//lv.wikipedia.org/" lang="lv">LatvieÅ¡u</a></li>
+<li><a href="//lb.wikipedia.org/" lang="lb">LÃ«tzebuergesch</a></li>
+<li><a href="//li.wikipedia.org/" lang="li">Limburgs</a></li>
+<li><a href="//lmo.wikipedia.org/" lang="lmo">Lumbaart</a></li>
+<li><a href="//mai.wikipedia.org/" lang="mai" title="MaithilÄ«">à¤®à¥ˆà¤¥à¤¿à¤²à¥€</a></li>
+<li><a href="//mg.wikipedia.org/" lang="mg">Malagasy</a></li>
+<li><a href="//ml.wikipedia.org/" lang="ml" title="Malayalam">à´®à´²à´¯à´¾à´³à´‚</a></li>
+<li><a href="//zh-classical.wikipedia.org/" lang="lzh" title="Man4jin4 / WÃ©nyÃ¡n">æ–‡è¨€</a></li>
+<li><a href="//mr.wikipedia.org/" lang="mr" title="Marathi">à¤®à¤°à¤¾à¤ à¥€</a></li>
+<li><a href="//xmf.wikipedia.org/" lang="xmf" title="Margaluri">áƒ›áƒáƒ áƒ’áƒáƒšáƒ£áƒ áƒ˜</a></li>
+<li><a href="//arz.wikipedia.org/" lang="arz" title="Maá¹£rÄ«"><bdi dir="rtl">Ù…ØµØ±Ù‰</bdi></a></li>
+<li><a href="//mzn.wikipedia.org/" lang="mzn" title="MÃ¤zeruni"><bdi dir="rtl">Ù…Ø§Ø²ÙØ±ÙˆÙ†ÛŒ</bdi></a></li>
+<li><a href="//cdo.wikipedia.org/" lang="cdo" title="Ming-deng-ngu">MÃ¬ng-dÄ•Ì¤ng-ngá¹³Ì„ / é–©æ±èªž</a></li>
+<li><a href="//mn.wikipedia.org/" lang="mn" title="Mongol">ÐœÐ¾Ð½Ð³Ð¾Ð»</a></li>
+<li><a href="//my.wikipedia.org/" lang="my" title="Myanmarsar">á€™á€¼á€”á€ºá€™á€¬á€˜á€¬á€žá€¬</a></li>
+<li><a href="//new.wikipedia.org/" lang="new" title="Nepal Bhasa">à¤¨à¥‡à¤ªà¤¾à¤² à¤­à¤¾à¤·à¤¾</a></li>
+<li><a href="//ne.wikipedia.org/" lang="ne" title="NepÄlÄ«">à¤¨à¥‡à¤ªà¤¾à¤²à¥€</a></li>
+<li><a href="//nap.wikipedia.org/" lang="nap">Nnapulitano</a></li>
+<li><a href="//oc.wikipedia.org/" lang="oc">Occitan</a></li>
+<li><a href="//mhr.wikipedia.org/" lang="mhr" title="Olyk Marij">ÐœÐ°Ñ€Ð¸Ð¹</a></li>
+<li><a href="//or.wikipedia.org/" lang="or" title="Oá¹›iÄ">à¬“à¬¡à¬¿à¬¼à¬†</a></li>
+<li><a href="//pa.wikipedia.org/" lang="pa" title="PaÃ±jÄbÄ« (GurmukhÄ«)">à¨ªà©°à¨œà¨¾à¨¬à©€ (à¨—à©à¨°à¨®à©à¨–à©€)</a></li>
+<li><a href="//pnb.wikipedia.org/" lang="pnb" title="PaÃ±jÄbÄ« (ShÄhmukhÄ«)"><bdi dir="rtl">Ù¾Ù†Ø¬Ø§Ø¨ÛŒ (Ø´Ø§Û Ù…Ú©Ú¾ÛŒ)</bdi></a></li>
+<li><a href="//ps.wikipedia.org/" lang="ps" title="PaÊ‚to"><bdi dir="rtl">Ù¾ÚšØªÙˆ</bdi></a></li>
+<li><a href="//pms.wikipedia.org/" lang="pms">PiemontÃ¨is</a></li>
+<li><a href="//nds.wikipedia.org/" lang="nds">PlattdÃ¼Ã¼tsch</a></li>
+<li><a href="//qu.wikipedia.org/" lang="qu">Runa Simi</a></li>
+<li><a href="//sa.wikipedia.org/" lang="sa" title="Saá¹ƒská¹›tam">à¤¸à¤‚à¤¸à¥à¤•à¥ƒà¤¤à¤®à¥</a></li>
+<li><a href="//sah.wikipedia.org/" lang="sah" title="Saxa Tyla">Ð¡Ð°Ñ…Ð° Ð¢Ñ‹Ð»Ð°</a></li>
+<li><a href="//sco.wikipedia.org/" lang="sco">Scots</a></li>
+<li><a href="//sq.wikipedia.org/" lang="sq">Shqip</a></li>
+<li><a href="//scn.wikipedia.org/" lang="scn">Sicilianu</a></li>
+<li><a href="//si.wikipedia.org/" lang="si" title="Siá¹ƒhala">à·ƒà·’à¶‚à·„à¶½</a></li>
+<li><a href="//sd.wikipedia.org/" lang="sd" title="SindhÄ«"><bdi dir="rtl">Ø³Ù†ÚŒÙŠ</bdi></a></li>
+<li><a href="//szl.wikipedia.org/" lang="szl">ÅšlÅ¯nski</a></li>
+<li><a href="//su.wikipedia.org/" lang="su">Basa Sunda</a></li>
+<li><a href="//sw.wikipedia.org/" lang="sw">Kiswahili</a></li>
+<li><a href="//tl.wikipedia.org/" lang="tl">Tagalog</a></li>
+<li><a href="//tt.wikipedia.org/" lang="tt">Ð¢Ð°Ñ‚Ð°Ñ€Ñ‡Ð° / TatarÃ§a</a></li>
+<li><a href="//te.wikipedia.org/" lang="te" title="Telugu">à°¤à±†à°²à±à°—à±</a></li>
+<li><a href="//bug.wikipedia.org/" lang="bug">á¨…á¨” á¨•á¨™á¨á¨— / Basa Ugi</a></li>
+<li><a href="//vec.wikipedia.org/" lang="vec">VÃ¨neto</a></li>
+<li><a href="//wa.wikipedia.org/" lang="wa">Walon</a></li>
+<li><a href="//wuu.wikipedia.org/" lang="wuu" title="WÃº YÇ”" data-convert-hans="å´è¯­" id="wuu_wiki">å³èªž</a></li>
+<li><a href="//yi.wikipedia.org/" lang="yi" title="YidiÅ¡"><bdi dir="rtl">×™×™Ö´×“×™×©</bdi></a></li>
+<li><a href="//yo.wikipedia.org/" lang="yo">YorÃ¹bÃ¡</a></li>
+<li><a href="//zh-yue.wikipedia.org/" lang="yue" title="Yuht YÃºh / Jyut6 jyu5" data-convert-hans="ç²¤è¯­" id="zh-yue_wiki">ç²µèªž</a></li>
+<li><a href="//diq.wikipedia.org/" lang="diq" title="Zazaki">Zazaki</a></li>
+<li><a href="//bat-smg.wikipedia.org/" lang="sgs">Å½emaitÄ—Å¡ka</a></li>
+</ul>
+</div>
+<!-- Bookshelves -->
+<h2 class="bookshelf-container">
+<span class="bookshelf">
+<span class="text">
+<bdi dir="ltr">
+1&nbsp;000+
+</bdi>
+<span class="jsl10n" data-jsl10n="entries">
+articles
+</span>
+</span>
+</span>
+</h2>
+
